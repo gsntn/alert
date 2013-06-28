a0d405e134cec31f7aa84652fd1efb460a7e8819
g1santana <gilibaldo@gmail.com>
2013-06-28 02:25:41 +0200

    first commit

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default[:alert][:dir]       = "/home/ubuntu"
+default[:alert][:file]       = "log.txt"
+default[:mailsend][:git]       = "https://github.com/gsntn/mailsend"
+default[:mailsend][:name]       = "mailsend-1.17b12.tar.gz"
